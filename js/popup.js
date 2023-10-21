471bdc38462b02cf4baff38e976e0d761ad73d47
nomandhoni-cs <nomandhoni@yahoo.com>
2023-10-22 00:40:25 +0600

    Changed the manifest version links and used github api

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-  chrome.runtime.getManifest().version;
-  const versionElement = document.getElementById("version");
-  versionElement.textContent = `Version: ${
-    chrome.runtime.getManifest().version
-  }`;
-});
+// Fetch the version number from the GitHub API
+fetch("https://api.github.com/repos/nomandhoni-cs/Facebook-QuietFeed/tags")
+  .then((response) => response.json())
+  .then((tags) => {
+    if (tags && tags.length > 0) {
+      const versionNumber = tags[0].name;
+      document.getElementById(
+        "version"
+      ).textContent = `Version: ${versionNumber}`;
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching version:", error);
+  });
